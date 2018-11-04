19cc68eaf14e9b6dbaab4c2af6bb29f480ad598c
agzpie <agata.pietralik@student.wat.edu.pl>
2018-11-04 22:40:41 +0100

    almost done, everything works, no main

--- main.cpp
+++ main.cpp
@@ -39,9 +39,22 @@ class List {
         int size;
 
     public:
-        void addToBeginning(int key);
-        void addToBeginning(ListElement &element) {
+        void addToBeginning(ListElement *element) {
+            if (firstElement == NULL) {
+                firstElement = element;
+                size++;
+                return;
+            }
 
+            ListElement *node = firstElement;
+            element->setNext(node);
+            firstElement = element;
+            size++;
+        }
+
+        void addToBeginning(int key) {
+            ListElement* element = new ListElement(key);
+            this->addToBeginning(element);
         }
 
         void addToEnd(ListElement *element) {
@@ -66,10 +79,80 @@ class List {
             this->addToEnd(element);
         }
 
-        ListElement* getElement(int key);
-        bool contains(int key);
-        void removeKey(int key);
-        void clearAll();
+        ListElement* getElement(int key) {
+            if (firstElement == NULL) {
+                return NULL;
+            }
+            ListElement *node = firstElement;
+            do {
+                if (node->getKey() == key) {
+                    return node;
+                }
+                node = node->getNext();
+            } while (node != NULL);
+            return NULL;
+        }
+
+        bool contains(int key) {
+            if (firstElement == NULL) {
+                return false;
+            }
+            ListElement *node = firstElement;
+            do {
+                if (node->getKey() == key) {
+                    return true;
+                }
+                node = node->getNext();
+            } while (node != NULL);
+            return false;
+        }
+        void removeKey(int key) {
+            if (firstElement == NULL) {
+                return;
+            }
+
+            ListElement *node = firstElement;
+            ListElement *nextNode;
+
+            if (firstElement->getKey() == key) {
+                nextNode = node->getNext();
+                firstElement = nextNode;
+                delete node;
+                size--;
+                return;
+            }
+
+            do {
+                nextNode = node->getNext();
+
+                if (nextNode->getKey() == key) {
+                    node->setNext(nextNode->getNext());
+                    delete nextNode;
+                    size--;
+                    return;
+                }
+
+                node = node->getNext();
+            } while (node != NULL);
+        }
+
+        void clearAll() {
+            if (firstElement == NULL) {
+                return;
+            }
+            ListElement *node = firstElement;
+            ListElement *nextNode;
+
+            do {
+                nextNode = node->getNext();
+                delete node;
+                node = nextNode;
+                size--;
+            } while (node != NULL);
+
+            this->firstElement = NULL;
+        }
+
         void printList() {
             if (firstElement == NULL) {
                 cout << "List = [] size = 0" << endl;
@@ -102,7 +185,33 @@ int main() {
     myList.addToEnd(3);
     myList.addToEnd(4);
 
+    cout << "LISTA:" << endl;
     myList.printList();
+
+    myList.clearAll();
+    cout << "deleteing it all:" << endl;
+    myList.printList();
+
+    myList.addToEnd(5);
+    myList.addToEnd(6);
+    myList.addToEnd(7);
+    myList.addToEnd(8);
+    myList.printList();
+    cout << "does it contain? ";
+    cout << myList.contains(5) << endl;
+    cout << "get element: ";
+    ListElement* elelel = myList.getElement(7);
+    cout << elelel->getKey() << endl;
+
+    cout << "deleting a key" << endl;
+    myList.removeKey(8);
+    myList.printList();
+
+    cout << "adding to the beginning" << endl;
+    myList.addToBeginning(1);
+    myList.printList();
+
+
 
 
     return 0;
